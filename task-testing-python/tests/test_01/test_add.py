7692655653bc21208316b0ed1f8930fe665df5dd
Dinara Zhussupova <zhus.dinara@gmail.com>
2023-03-10 14:17:05 +0600

    edited some tests for functions

--- task-testing-python/tests/test_01/test_add.py
+++ task-testing-python/tests/test_01/test_add.py
@@ -3,7 +3,13 @@ from simple_library_01.functions import add
 
 def test_add():
     assert 4 == add(2, 2)
+
+def test_add_not_equal():
     assert 100 == add(28, 72)
+
+def test_add_with_zero():
     assert 12 == add(0, 12)
-    assert -43 == add(-10, -33)
+
+def test_add_negative():
+    assert -38 == add(-17, -21)
 
